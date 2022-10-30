3c785d974ade88ce5b91e1564c9846856042a0f2
sunny <sunnyroy8084401051@gmail.com>
2022-10-30 17:44:34 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+In this section we will learn about Odd and Even number ,now we are going to write a code in which we will se
+about how to find that the number is even or odd.
+___________________________________________________________________________________________________________________
+Approach : first take the input from user and divide the input to 2 if the input number is divisible by 2 then the
+ number is even else number is odd.
+"""
+A = int (input("enter the first number:"))
+# B = int (input("enter the second number:"))
+if(A/2 == 0):
+  print("Number is even")
+else:
+  print("Number is odd")
